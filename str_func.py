5520eb3e88d25f4c978e40485e0447c4b8f3d876
Alina Khusainova <vlinv.khusainova23@gmail.com>
2023-12-27 19:50:57 +0300

    add new funcs

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upper_sentences(sentence):
-    """функция переводит все буквы в верхний регистр"""
-   return sentence.upper()
+    """Функция переводит все буквы в верхний регистр."""
+    return sentence.upper()
+
+def title_sentences(sentence):
+    """Функция делает заглавными первые буквы в словах."""
+    return sentence.title()
